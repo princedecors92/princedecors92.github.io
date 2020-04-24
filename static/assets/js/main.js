865ff36449893dabde145b358db0be2fa8100f53
elavarasan <princedecors92@gmail.com>
2020-04-24 14:20:52 +0530

    mobile navigation

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -49,7 +49,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('ri-menu-line icofont-close');
           $('.mobile-nav-overly').fadeOut();
         }
         return false;
@@ -63,12 +63,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="ri-menu-line"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').toggleClass('ri-menu-line icofont-close');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -83,7 +83,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('ri-menu-line icofont-close');
           $('.mobile-nav-overly').fadeOut();
         }
       }
