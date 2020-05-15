d9d87f94ffffc95e2f7370276099076cc4f69c73
elavarasan <princedecors92@gmail.com>
2020-05-15 14:23:16 +0530

    fb ad

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,3 +1,14 @@
+!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '938378916614518');
+  fbq('track', 'PageView');
+  
 !(function($) {
 	//example url to view card http://localhost:8080/viewCard?user_name=elavarasan
 if(localStorage.token){
