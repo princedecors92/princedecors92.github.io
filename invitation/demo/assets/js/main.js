1c28d0883fa8e3aed5090c5817eba76ae97fa3a4
elavarasan <princedecors92@gmail.com>
2020-06-21 23:02:33 +0530

    dd

--- invitation/demo/assets/js/main.js
+++ invitation/demo/assets/js/main.js
@@ -16,13 +16,13 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // Output the result in an element with id="countDown"
+  document.getElementById("countDown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countDown").innerHTML = "EXPIRED";
   }
 }, 1000);
